0dc4bd800356e83918d21cc7613e8e6274a2a7da
Winkermann <willmeschke@gmail.com>
2023-11-23 14:14:36 -0300

    first component - clientHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
 import './App.css';
 import computerImg from '././assets/computer.png';
-import cartIcon from '././assets/cart.svg';
+import HeaderClient from './components/HeaderClient';
 
 function App() {
   return (
     <>
-      <header className="dsc-header-client">
-      <nav className="dsc-container">
-        <h1>DSCommerce</h1>
-        <div className="dsc-navbar-right">
-          <div className="dsc-menu-items-container">
-            <div className="dsc-menu-item">
-              <img src={cartIcon} alt="Carrinho de compras" />
-            </div>
-          </div>
-          <a href="#">Entrar</a>
-        </div>
-      </nav>
-    </header>
+    <HeaderClient />  
     <main>
       <section id="product-details-section" className="dsc-container">
         <div className="dsc-card dsc-mb20">
