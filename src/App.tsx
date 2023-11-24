5a628c929ff4437a5ed162d7a5fc2e04ca1bc6a0
Winkermann <willmeschke@gmail.com>
2023-11-24 11:24:45 -0300

    Catalog page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import ProductDetails from './routes/ProductDetails';
+import Catalog from './routes/Catalog';
+// import ProductDetails from './routes/ProductDetails';
 
 function App() {
   return (
-    <ProductDetails />
+    <Catalog />
   )
 }
 
---
+++ src/components/ButtonNextPage/index.tsx
@@ -0,0 +1,5 @@
+import "./styles.css";
+
+export default function ButtonNextPage() {
+  return <div className="dsc-btn-next-page">Carregar mais</div>;
+}
