beb24f1d6171698761c3e881b8fc1628e7c4ecf8
Winkermann <willmeschke@gmail.com>
2023-11-27 15:32:45 -0300

    adding react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-// import Catalog from './routes/Catalog';
-import Catalog from './routes/Catalog';
-// import ProductDetails from './routes/ProductDetails';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Catalog from "./routes/ClientHome/Catalog";
+import ProductDetails from "./routes/ClientHome/ProductDetails";
+import ClientHome from "./routes/ClientHome";
 
 function App() {
   return (
-    <Catalog />
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientHome />}>
+          <Route index element={<Catalog />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="product-details" element={<ProductDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
